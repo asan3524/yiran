547a4a076c5656a6e4f21ce556f176806650d9e2
lishibang <lsb3524@163.com>
2019-03-19 14:54:43 +0800

    base init

---
+++ yiran-base/base-eureka/src/main/java/com/ddsh/base/eureka/config/WebSecurityConfigurer.java
@@ -0,0 +1,23 @@
+package com.ddsh.base.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+		http.csrf().disable();
+		// 注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/
+		// 这种方式登录,所以必须是httpBasic,
+		// 如果是form方式,不能使用url格式登录
+		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+	}
+}
