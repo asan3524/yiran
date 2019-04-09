3f3322565e63a72784af33bd9cfd7f005d7748c4
lishibang <lsb3524@163.com>
2019-04-09 15:12:43 +0800

    demo

--- yiran-demo/demo-provider/src/main/java/com/yiran/demo/provider/Application.java
+++ yiran-demo/demo-provider/src/main/java/com/yiran/demo/provider/Application.java
@@ -3,14 +3,11 @@ package com.yiran.demo.provider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
-
-@SpringBootApplication(scanBasePackages = {"com.yiran.*"})
-@EnableFeignClients({ "com.yiran" })
+@SpringBootApplication(scanBasePackages = { "com.yiran" })
 @EnableDiscoveryClient
-@EnableResourceServer
+@EnableOAuth2Client
 public class Application {
 
 	public static void main(String[] args) {
