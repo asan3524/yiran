f9ada3dbcab419c93fbac16cbe8703d6a0d5b59a
lishibang <lsb3524@163.com>
2019-04-12 15:38:41 +0800

    system

--- yiran-base/base-system/system-restapi/src/main/java/com/yiran/base/system/controller/LoadController.java
+++ yiran-base/base-system/system-restapi/src/main/java/com/yiran/base/system/controller/LoadController.java
@@ -16,9 +16,29 @@ public class LoadController extends BaseController {
 	@Autowired
 	private LoadService loadService;
 
+	@RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST })
+	public Object load() {
+		BaseRespData result = loadService.loadUser();
+		result = loadService.loadRole();
+		result = loadService.loadResource();
+		return response(result);
+	}
+
 	@RequestMapping(value = "/user", method = { RequestMethod.GET, RequestMethod.POST })
 	public Object loadUser() {
 		BaseRespData result = loadService.loadUser();
+		return response(result);
+	}
+
+	@RequestMapping(value = "/role", method = { RequestMethod.GET, RequestMethod.POST })
+	public Object loadRole() {
+		BaseRespData result = loadService.loadRole();
+		return response(result);
+	}
+
+	@RequestMapping(value = "/resource", method = { RequestMethod.GET, RequestMethod.POST })
+	public Object loadResource() {
+		BaseRespData result = loadService.loadResource();
 		return response(result);
 	}
 }
