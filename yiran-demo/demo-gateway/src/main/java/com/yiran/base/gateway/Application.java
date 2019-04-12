6a196b77025cb95c205981e24b3a75f82faf744d
lishibang <lsb3524@163.com>
2019-04-12 11:08:31 +0800

    规范第一轮release的版本依赖

--- yiran-demo/demo-gateway/src/main/java/com/yiran/base/gateway/Application.java
+++ yiran-demo/demo-gateway/src/main/java/com/yiran/base/gateway/Application.java
@@ -1,9 +1,11 @@
-package com.yiran.base.oauth2;
+package com.yiran.base.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class Application {
 
 	public static void main(String[] args) {
