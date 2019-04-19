602e7bdf69c57eae9c12b572ebacaa10b807dd35
lishibang <lsb3524@163.com>
2019-04-19 16:35:15 +0800

    gateway服务

---
+++ yiran-base/base-gateway/src/main/java/com/yiran/base/gateway/Application.java
@@ -0,0 +1,14 @@
+package com.yiran.base.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(scanBasePackages = { "com.yiran" })
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
