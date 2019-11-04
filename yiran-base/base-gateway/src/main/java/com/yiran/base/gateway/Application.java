1c34314019a63fea481eb1804661553ab087fabf
lishibang <lsb3524@163.com>
2019-11-04 21:44:34 +0800

    gateway通过外置filter的方式自定义校验 此处把湖南项目中外置的yjgk-filter去除

--- yiran-base/base-gateway/src/main/java/com/yiran/base/gateway/Application.java
+++ yiran-base/base-gateway/src/main/java/com/yiran/base/gateway/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = {"com"})
-@EnableFeignClients(basePackages = "com")
 @EnableDiscoveryClient
 @EnableZuulProxy
 public class Application {
